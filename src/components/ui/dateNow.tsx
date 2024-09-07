2e55da63933a93ca86d22e2c41bfd1965978e83e
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2024-09-06 21:37:09 -0500

    chore: Refactor clientDate implementation in UI components

--- src/components/ui/dateNow.tsx
+++ src/components/ui/dateNow.tsx
@@ -1,13 +1,16 @@
+import moment from "moment";
 import React from "react";
 
 const ClientDateNow = () => {
-  const clientDate = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const dateString = clientDate.toString();
-  const localeString = clientDate.toLocaleString();
+  const newMoment = moment();
+  const clientDate = newMoment.format();
+  const newDate = new Date(clientDate);
+  const dateString = newDate.toString();
+  const localeString = newDate.toLocaleString();
 
   return (
     <div className="flex flex-col">
-      <p>{JSON.stringify(clientDate)}</p>
+      <p>{JSON.stringify(newDate)}</p>
       <p>{dateString}</p>
       <p>{localeString}</p>
     </div>
