5eba1c605a8f4887dd473493a1497d23309f477c
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2024-09-15 21:47:42 -0500

    feat: added new species

--- src/db/especies.ts
+++ src/db/especies.ts
@@ -1,63 +1,66 @@
 const especie1 = {
   id: 1,
-  nombre: "Perro",
-  descripcion: "Animal doméstico",
+  nombre: "Elefante africano (Loxodonta africana)",
+  descripcion:
+    "El elefante africano es el mamífero terrestre más grande del mundo, nativo de las sabanas, bosques y desiertos de África subsahariana. Se caracteriza por sus grandes orejas, que ayudan a regular su temperatura corporal, y su trompa, una herramienta versátil utilizada para alimentarse, beber agua y comunicarse. Los elefantes africanos son conocidos por su compleja estructura social y su gran inteligencia, habitan en áreas protegidas y reservas naturales debido a la caza furtiva y la pérdida de hábitat.",
   tasaNatalidadPromedio: {
     joven: 0,
-    juvenil: 0.2,
-    adulto: 0.3,
+    juvenil: 0.02,
+    adulto: 0.15,
   },
   probabilidadDeSupervivencia: {
-    joven_juvenil: 0.8,
-    juvenil_adulto: 0.7,
-    permanecer_adulto: 1,
+    joven_juvenil: 0.5,
+    juvenil_adulto: 0.85,
+    permanecer_adulto: 0.9,
   },
   poblacionInicial: {
-    joven: 100,
-    juvenil: 50,
-    adulto: 25,
+    joven: 1000,
+    juvenil: 800,
+    adulto: 600,
   },
 };
 
 const especie2 = {
   id: 2,
-  nombre: "Gato",
-  descripcion: "Animal doméstico",
+  nombre: "Águila calva (Haliaeetus leucocephalus)",
+  descripcion:
+    "El águila calva es una majestuosa ave rapaz nativa de América del Norte, que habita en áreas cercanas a grandes cuerpos de agua, como lagos y ríos, donde encuentra su principal fuente de alimento: el pescado. Se caracteriza por su plumaje marrón oscuro, cabeza y cola blancas, y su fuerte pico amarillo. El águila calva es un símbolo nacional de los Estados Unidos, y su conservación ha sido exitosa tras años de declive debido a la caza y el uso de pesticidas.",
   tasaNatalidadPromedio: {
     joven: 0,
-    juvenil: 0.2,
-    adulto: 0.3,
+    juvenil: 0.05,
+    adulto: 0.25,
   },
   probabilidadDeSupervivencia: {
-    joven_juvenil: 0.8,
+    joven_juvenil: 0.4,
     juvenil_adulto: 0.7,
-    permanecer_adulto: 1,
+    permanecer_adulto: 0.8,
   },
   poblacionInicial: {
-    joven: 100,
-    juvenil: 50,
-    adulto: 25,
+    joven: 500,
+    juvenil: 300,
+    adulto: 200,
   },
 };
 
 const especie3 = {
   id: 3,
-  nombre: "Pez",
-  descripcion: "Animal doméstico",
+  nombre: "Tortuga marina (Chelonioidea)",
+  descripcion:
+    "Las tortugas marinas son reptiles longevos que habitan los océanos tropicales y subtropicales de todo el mundo. Se caracterizan por su caparazón duro y aplanado, adaptado para nadar largas distancias, y sus aletas en lugar de patas, que las impulsan en el agua. Migran grandes distancias para anidar en playas específicas, donde desovan. A pesar de su longevidad, muchas especies de tortugas marinas están en peligro de extinción debido a la contaminación, la pesca incidental y la destrucción de sus áreas de anidación.",
   tasaNatalidadPromedio: {
     joven: 0,
-    juvenil: 0.2,
+    juvenil: 0,
     adulto: 0.3,
   },
   probabilidadDeSupervivencia: {
-    joven_juvenil: 0.8,
-    juvenil_adulto: 0.7,
-    permanecer_adulto: 0,
+    joven_juvenil: 0.1,
+    juvenil_adulto: 0.5,
+    permanecer_adulto: 0.85,
   },
   poblacionInicial: {
-    joven: 100,
-    juvenil: 50,
-    adulto: 25,
+    joven: 10000,
+    juvenil: 1000,
+    adulto: 500,
   },
 };
 
